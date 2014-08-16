a809789c631539140de56fd5e26e87e23ad08dfd
a8568730 <a8568730@gmail.com.tw>
2014-08-16 10:33:36 +0800

    第一次跑過所有textgrid

--- NativeDB_py/取出多次聲音位置.py
+++ NativeDB_py/取出多次聲音位置.py
@@ -1,10 +1,10 @@
 
 
-切割檔 = open('Penang_Y_M01_01.TextGrid', encoding="utf-8")
+切割檔 = open('textgrid/Penang_Y_F01_DiT.TextGrid', encoding="utf-8")
 資料 = 切割檔.readlines()
 切割檔.close()
 
-寫出結果 = open('output_Y_M01_01.txt', 'w', encoding="utf-8")
+寫出結果 = open('outputs/Penang_Y_F01_DiT.txt', 'w', encoding="utf-8")
 
 # 889 from "intervals: size = 889"
 區間數 = 資料[13].split(' ')[-2]
