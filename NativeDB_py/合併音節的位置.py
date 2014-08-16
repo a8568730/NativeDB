f29f6a6b9be24422d0760cac3ee017aaa31c2d88
a8568730 <a8568730@gmail.com.tw>
2014-08-16 12:57:03 +0800

    試驗都寫好了

--- NativeDB_py/合併音節的位置.py
+++ NativeDB_py/合併音節的位置.py
@@ -1,25 +1,24 @@
 
-
-fp = open('outputs/Penang_Y_M01_01.txt',	encoding='utf-8')
-
-
-#	看到第一個xxx時, 下一行C的值要記錄min
-#	直到看到第二個xxx, 最後一行的值要紀錄max
-
-xxx	= 0
-sylmin = 1000000.0
-sylmax = -100.0
-syl = []
-for	line in fp:
-	名字,	xmin, xmax = line.strip().split(' ')
-	xmin = float(xmin)
-	xmax = float(xmax)
-	if(名字=="XXX" and xxx==0):
-		xxx = 1
-	elif(名字=="XXX" and xxx==1):
-		print(','.join(syl), sylmin, sylmax)
-		syl = []
-	else:
-		sylmin = xmin if sylmin > xmin else sylmin;
-		sylmax = xmax if sylmax < xmax else sylmax;
-		syl.append(名字)
+def 合併位置(資料):
+	#	看到第一個xxx時, 下一行C的值要記錄min
+	#	直到看到第二個xxx, 最後一行的值要紀錄max
+	
+	xxx	= 0
+	sylmin = 1000000.0
+	sylmax = -100.0
+	syl = []
+	寫出結果 = []
+	for	line in 資料:
+		名字,	xmin, xmax = line
+		xmin = float(xmin)
+		xmax = float(xmax)
+		if(名字=="XXX" and xxx==0):
+			xxx = 1
+		elif(名字=="XXX" and xxx==1):
+			寫出結果.append((','.join(syl), sylmin, sylmax))
+			syl = []
+		else:
+			sylmin = xmin if sylmin > xmin else sylmin;
+			sylmax = xmax if sylmax < xmax else sylmax;
+			syl.append(名字)
+	return 寫出結果		
