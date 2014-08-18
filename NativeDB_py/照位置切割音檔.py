c4048858ea35a4203e1e64a14c5b474bbca0d76f
wenli <a8568730@hotmail.com>
2014-08-18 15:39:50 +0800

    Fixes #1 切割前，確認切割的開頭和結尾位置是合法的

--- NativeDB_py/照位置切割音檔.py
+++ NativeDB_py/照位置切割音檔.py
@@ -10,14 +10,21 @@ def 切割音檔(音檔路徑, 合併後的資料):
 		frameRate = origAudio.getframerate()
 		nChannels = origAudio.getnchannels()
 		sampWidth = origAudio.getsampwidth()
-		start = float(一筆資料[1]) - 0.2
-		end = float(一筆資料[2]) + 0.1
-
+		nFrames = origAudio.getnframes()
+		
+# 		0.2和0.1是隨便給的
+		start = int((float(一筆資料[1]) - 0.2)*frameRate)
+		end = int((float(一筆資料[2]) + 0.1)*frameRate)
+		
+#	 	若是輸入的切割位置超出了音檔的長度，就切齊。
+		if(start < 0):
+			start = 0
+		if(end > nFrames):
+			end = nFrames		
 		anchor = origAudio.tell()
-		origAudio.setpos(anchor + int(start*frameRate))
-		chunkData = origAudio.readframes(int((end-start)*frameRate))
-		
-		
+		origAudio.setpos(anchor + start)
+		chunkData = origAudio.readframes(end-start)
+			
 		音檔原名 = os.path.splitext(音檔路徑)[0]
 		輸出名 = 音檔原名 + '_chunk.wav' 
 		chunkAudio = wave.open(輸出名,'w')
@@ -28,5 +35,3 @@ def 切割音檔(音檔路徑, 合併後的資料):
 		
 		chunkAudio.close()
 		origAudio.close()
-
-		
