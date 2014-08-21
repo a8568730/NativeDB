e6786cc76a5ddb6843a59fda31d8972c926ecd0d
wenli <a8568730@hotmail.com>
2014-08-21 14:44:33 +0800

    目前跑過單詞和雙詞測試了

--- NativeDB_py/把xlsx的陣列轉成json.py
+++ NativeDB_py/把xlsx的陣列轉成json.py
@@ -9,7 +9,7 @@ def xlsx陣列轉json(xlsx陣列, 詞數):
 		return 'xlsx檔要四個欄位: Num, Word, IPA, Note'
 
 # 	第一行是否為這四個欄位:'Num','Word','IPA', Note
-	預期欄位 = ['Num','Word','IPA', 'Note']
+	預期欄位 = ["Num","Word","IPA", "Note"]
 	for 索引 in range(欄數):
 		if(xlsx陣列[0][索引] != 預期欄位[索引]):
 			return 'col(1,' + str(索引) + ')應為' + 預期欄位[索引]
@@ -42,13 +42,12 @@ def xlsx陣列轉json(xlsx陣列, 詞數):
 		if(詞數 != len(字)):
 			regex = re.compile('\(.*?\)')
 			去掉括號後的字們 = regex.sub(',', 字)
-			print(去掉括號後的字們)
+
 			
 			if(去掉括號後的字們==''):
 				return 'col(' + str(索引+1) + ')的字格式不符'
 			
 			括號外的字陣列 = 去掉括號後的字們.split(',')
-			print(括號外的字陣列)
 			有符合詞數 = False
 			for 一個 in 括號外的字陣列:
 				if(len(一個) == 詞數):
@@ -62,10 +61,10 @@ def xlsx陣列轉json(xlsx陣列, 詞數):
 			return 'col(' + str(索引+1) + ')的IPA欄位空白'
 		
 		寫出結果.append(
-				{'Name':編號字 + ("{0:0"+str(初始編號數字長度)+"d}").format(索引+1),
-				'Word':字, 
-				'IPA':音標, 
-				'Note':註記
+				{預期欄位[0]: 編號字 + ("{0:0"+str(初始編號數字長度)+"d}").format(索引+1),
+				預期欄位[1]: 字, 
+				預期欄位[2]: 音標,
+				預期欄位[3]:註記
 			});	
-	return 'OK'
-
+# 	return 'OK'
+	return 寫出結果
--- 試驗/試驗_陣列轉json.py
+++ 試驗/試驗_陣列轉json.py
@@ -13,8 +13,11 @@ class 基本元素試驗(unittest.TestCase):
 				['Num','Word','IPA','Note'],
 				['Zhang_VT_001','(乖)巧','kʰa3','kʰa3']
 			]
-# 		答案 =  "[{Num:'Zhang_VT_001',Word:'(乖)巧',	IPA:'kʰa3',Note:'kʰa3'}]"
-		答案 = 'OK'
+		答案 =  [{"Num":"Zhang_VT_001",
+				"Word":"(乖)巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"}]
+# 		答案 = 'OK'
 		結果 = xlsx陣列轉json(問題, 1)
 		self.assertEqual(答案, 結果)
 		
@@ -56,7 +59,16 @@ class 基本元素試驗(unittest.TestCase):
 				['Zhang2_VT_001','(乖)巧','kʰa3','kʰa3'],
 				['Zhang2_VT_002','(乖)巧','kʰa3','kʰa3']
 			]
-		答案 =  'OK'
+# 		答案 =  'OK'
+		答案 =  [{"Num":"Zhang2_VT_001",
+				"Word":"(乖)巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+				{"Num":"Zhang2_VT_002",
+				"Word":"(乖)巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+			]
 		詞數 = 1
 		結果 = xlsx陣列轉json(問題, 詞數)
 		self.assertEqual(答案, 結果)
@@ -118,21 +130,25 @@ class 基本元素試驗(unittest.TestCase):
 		self.assertEqual(答案, 結果)	
 		
 	
-	def test_單詞格式右(self):
-		問題 = [
-				['Num','Word','IPA','Note'],
-				['Zhang_VT_001','(乖)巧','kʰa3','kʰa3']
-			]
-		答案 =  'OK'
-		結果 = xlsx陣列轉json(問題, 1)
-		self.assertEqual(答案, 結果)
-	
+# 	def test_單詞格式右(self):
+# 		問題 = [
+# 				['Num','Word','IPA','Note'],
+# 				['Zhang_VT_001','(乖)巧','kʰa3','kʰa3']
+# 			]
+# 		答案 =  'OK'
+# 		結果 = xlsx陣列轉json(問題, 1)
+# 		self.assertEqual(答案, 結果)
+# 	
 	def test_單詞格式左(self):
 		問題 = [
 				['Num','Word','IPA','Note'],
 				['Zhang_VT_001','巧(乖)','kʰa3','kʰa3']
 			]
-		答案 =  'OK'
+# 		答案 =  'OK'
+		答案 = [{"Num":"Zhang_VT_001",
+				"Word":"巧(乖)",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"}]
 		結果 = xlsx陣列轉json(問題, 1)
 		self.assertEqual(答案, 結果)
 	
@@ -141,7 +157,10 @@ class 基本元素試驗(unittest.TestCase):
 				['Num','Word','IPA','Note'],
 				['Zhang_VT_001','巧','kʰa3','kʰa3']
 			]
-		答案 =  'OK'
+		答案 = [{"Num":"Zhang_VT_001",
+				"Word":"巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"}]
 		結果 = xlsx陣列轉json(問題, 1)
 		self.assertEqual(答案, 結果)
 	
@@ -159,17 +178,29 @@ class 基本元素試驗(unittest.TestCase):
 				['Num','Word','IPA','Note'],
 				['Zhang_VT_001','(喔)巧(乖)','kʰa3','kʰa3']
 			]
-		答案 =  'OK'
+		答案 = [{"Num":"Zhang_VT_001",
+				"Word":"(喔)巧(乖)",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"}]
 		結果 = xlsx陣列轉json(問題, 1)
 		self.assertEqual(答案, 結果)
-		
+# 		
 	def test_雙詞格式純(self):
 		問題 = [
 				['Num','Word','IPA','Note'],
 				['Zhang_VT_001','乖巧','kʰa3','kʰa3'],
 				['Zhang_VT_002','喔喔','kʰa3','kʰa3']
 			]
-		答案 =  'OK'
+# 		答案 =  'OK'
+		答案 =  [{"Num":"Zhang_VT_001",
+				"Word":"乖巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+				{"Num":"Zhang_VT_002",
+				"Word":"喔喔",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+			]
 		詞數 = 2
 		結果 = xlsx陣列轉json(問題, 詞數)
 		self.assertEqual(答案, 結果)
@@ -180,7 +211,16 @@ class 基本元素試驗(unittest.TestCase):
 				['Zhang_VT_001','(喔)乖巧','kʰa3','kʰa3'],
 				['Zhang_VT_002','喔喔()','kʰa3','kʰa3']
 			]
-		答案 =  'OK'
+# 		答案 =  'OK'
+		答案 =  [{"Num":"Zhang_VT_001",
+				"Word":"(喔)乖巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+				{"Num":"Zhang_VT_002",
+				"Word":"喔喔()",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+			]
 		詞數 = 2
 		結果 = xlsx陣列轉json(問題, 詞數)
 		self.assertEqual(答案, 結果)
