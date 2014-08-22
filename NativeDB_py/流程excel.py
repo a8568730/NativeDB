cfdd18df283ce869ea2c918c9487a13bb9aa7760
蔡文莉 <a8568730@gmail.com>
2014-08-22 21:31:21 +0800

    處理掉每行開頭多餘空白

--- NativeDB_py/流程excel.py
+++ NativeDB_py/流程excel.py
@@ -4,20 +4,20 @@ from NativeDB_py.寫出json檔 import 寫出json檔
 import json
 
 
-def is_json(myjson):
-	try:
-		json_object = json.loads(myjson)
-	except:
-		return False
-	return True
- 
- 
+# def is_json(myjson):
+# 	try:
+# 		json_object = json.loads(myjson)
+# 	except:
+# 		return False
+# 	return True
+
+
 excel檔名 = 'Penang_Y_M01_correct.xlsx'
 xlsx陣列 = 把EXCEL讀進來(excel檔名)
 if(len(xlsx陣列) != 0):
 	json陣列 = xlsx陣列轉json(xlsx陣列, 1)
 	if(type(json陣列[0]) is dict):
- 		寫出json檔(json陣列, excel檔名)
+		寫出json檔(json陣列, excel檔名)
 # 	print(json陣列)
 # 	result= json.loads("[{\"num\":\"100\"}]")
 # 	print(result)
