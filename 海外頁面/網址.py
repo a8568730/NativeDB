8e037010e54d2ea4a57cbd58a3f3ce0303444c3b
蔡文莉 <a8568730@gmail.com>
2014-09-05 10:11:56 +0800

    加入主頁和描述頁

--- 海外頁面/網址.py
+++ 海外頁面/網址.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
 from 海外頁面.介面 import 首頁
+from 海外頁面.介面 import 細節描述頁
+from 海外頁面.介面 import 主頁
 
 
 urlpatterns = patterns('',
-	url(r'^.*$', 首頁, name='首頁'),
-
+	url(r'^index$', 主頁, name='主頁'),
+	url(r'^details$', 細節描述頁, name='細節描述頁'),
+	url(r'^.*$', 首頁, name='首頁')
 )
