775e9face09c5f2f7ad5c0e64fcc9f4f303018ba
薛丞宏 <ihcaoe@gmail.com>
2014-09-10 17:56:03 +0800

    正在改用ui-bootstrap的tabs

--- 海外頁面/static/js/first.js
+++ 海外頁面/static/js/first.js
@@ -1,9 +1,16 @@
 // Code goes here
 
-var app = angular.module("app1",[]);
+var app = angular.module("app1",['ui.bootstrap']);
 
 app.controller("LangController",["$scope", "$log", function($scope, $log){
   $scope.langs = ["華語", "閩語", "客語"];
+  $scope.call = function(lang){
+	  $log.log(lang);
+  };
+}]);
+
+app.controller("indexController",["$scope", "$log", function($scope, $log){
+  $scope.tabs = [{title:"華語"}, {title:"閩語"}, {title:"客語"}];
   $scope.call = function(lang){
 	  $log.log(lang);
   };
