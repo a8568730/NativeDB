766053f5e409b14efe647294b596889e3a14a654
薛丞宏 <ihcaoe@gmail.com>
2014-09-10 21:10:23 +0800

    『/全部語言json』印出目前資料庫語言表全部的語言

--- 海外頁面/介面.py
+++ 海外頁面/介面.py
@@ -13,6 +13,7 @@ from django.db.models import Q
 from django.views.generic.base import View
 from 海外頁面.表格 import 顯示語言表表格
 from 海外頁面.模型 import 語言表
+import json
 
 def 首頁(request):
 # 	output = ', '.join([p.title for p in latest_poll_list])
@@ -44,7 +45,8 @@ def 加語言表表格(request):
 		語言表格 = 顯示語言表表格(request.POST)  # A form bound to the POST data
 		if 語言表格.is_valid():
 			語言 = 語言表格.save()
-# 			return redirect('加語言表表格')
+			#	redirect 屬於GET方法
+			return redirect('加語言表表格')
 	else:
 		語言表格 = 顯示語言表表格()
 # 	template = loader.get_template('海外頁面/新文章.html')
@@ -58,3 +60,9 @@ def 加語言表表格(request):
 		'語言': 語言表格,
 		'揣著語言':揣著語言,
 	})
+
+def 加語言表表格json(request):
+	全部語言=[]
+	for 揣著語言 in 語言表.objects.all():
+		全部語言.append(揣著語言.語言)
+	return HttpResponse(json.dumps(全部語言), content_type="application/json")
--- 海外頁面/網址.py
+++ 海外頁面/網址.py
@@ -3,12 +3,15 @@ from 海外頁面.介面 import 首頁
 from 海外頁面.介面 import 細節描述頁
 from 海外頁面.介面 import 主頁
 from 海外頁面.介面 import 加語言表表格
+from 海外頁面.介面 import 加語言表表格json
 
 
 urlpatterns = patterns('',
 	url(r'^index$', 主頁, name='主頁'),
 	url(r'^details$', 細節描述頁, name='細節描述頁'),
-	url(r'^加語言表表格$',加語言表表格, name='加語言表表格'),
-	url(r'^.*$', 首頁, name='首頁')
 	
+	url(r'^加語言表表格$',加語言表表格, name='加語言表表格'),
+	url(r'^加語言表表格json$',加語言表表格json, name='加語言表表格json'),
+	
+	url(r'^.*$', 首頁, name='首頁')
 )
