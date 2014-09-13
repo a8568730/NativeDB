1864c7f4f5753199d61697689c5804870ed0f3b1
蔡文莉 <a8568730@gmail.com>
2014-09-13 22:34:03 +0800

    不用routeParam，改用宣告標籤傳入網址資料

--- 海外頁面/static/js/first.js
+++ 海外頁面/static/js/first.js
@@ -2,6 +2,15 @@
 
 var app = angular.module("app1",['ui.bootstrap','ngRoute']);
 
+app.directive('languageFlag', function() {
+	return function($scope, element, attrs) {
+		//		observe data-sui2
+	    attrs.$observe('sui2', function(value) {
+	        $scope.sui2sui2 = value;
+	    });
+	}
+});
+
 app.controller("LangController",["$scope", "$log","$http", function($scope, $log, $http){
 	$scope.langs = [];
 	$http({
@@ -20,40 +29,42 @@ app.controller("LangController",["$scope", "$log","$http", function($scope, $log
 
 app.controller("indexController",["$scope", "$log","$http","$routeParams", "$route", 
 	function($scope, $log, $http,$routeParams, $route){
-//		$scope.langs = [];
-		//		inject $route and $routeParams to get url params
-		// 		index/媠媠媠
-		$scope.param = $routeParams;
-		console.log($scope.param);	
-		
+		//		Method 1 :
+		//			inject $route and $routeParams to get url params
+		// 			index/媠媠媠
+		//			$scope.param = $routeParams;
+		//			isactive = (data[i] == $scope.param.language) ? true : false;
+	
+		//		Method 2 :	directive
 		$scope.tabs = [];
 		$http({
 				method: 'GET',
 				url: '/語言表全部json',
 				data: {}
 		}).success(function(data, status) {
-//				$scope.langs = data;
 				var isactive;
 				for(var i=0; i<data.length; i++){
-					isactive = (data[i]==$scope.param.language) ? true : false;
+					isactive = (data[i] == $scope.sui2sui2) ? true : false;
 					$scope.tabs.push({"lang":data[i], "active": isactive});
 					console.log({"lang":data[i], "active": isactive});
 				};
 				console.log($scope.tabs);
+				console.log('sui2=' + $scope.sui2sui2);
 		});
 		
 }]);
 
-app.config(function($routeProvider, $locationProvider) {
-	  $routeProvider
-	  	.when('/index/', {redirectTo: '/index/豬豬語'})
-	  	.when('/index/:language', {
-	    controller: 'indexController'
-	  });
 
-	  // configure html5 to get links working on jsfiddle
-	  $locationProvider.html5Mode(true);
-	});
+//		Method 1 :
+//			inject $route and $routeParams to get url params
+//app.config(function($routeProvider, $locationProvider) {
+//	  $routeProvider
+//	  	.when('/index/', {redirectTo: '/index/豬豬語'})
+//	  	.when('/index/:language', {
+//	    controller: 'indexController'
+//	  });
+//	  $locationProvider.html5Mode(true);
+//});
 
 /* 解決 Django 和 AngularJS共用{{}}的混淆問題 */
 app.config(function($interpolateProvider) {
