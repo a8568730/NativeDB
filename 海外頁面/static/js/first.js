35810275e17f562e9e9cf3a128d6b7e56eead174
蔡文莉 <a8568730@gmail.com>
2014-09-13 12:24:03 +0800

    index.html可以吃到網址了

--- 海外頁面/static/js/first.js
+++ 海外頁面/static/js/first.js
@@ -12,18 +12,11 @@ app.controller("LangController",["$scope", "$log","$http", function($scope, $log
 			$scope.langs = data;
 	});
 
-
 	$scope.call = function(lang){
 		$log.log(lang);
 	};
 }]);
 
-app.controller("indexController",["$scope", "$log", function($scope, $log){
-	$scope.tabs = [{title:"華語"}, {title:"閩語"}, {title:"客語"}];
-	$scope.call = function(lang){
-		$log.log(lang);
-	};
-}]);
 
 /* 解決 Django 和 AngularJS共用{{}}的混淆問題 */
 app.config(function($interpolateProvider) {
