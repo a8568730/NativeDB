60556ec9b18a2d74863df6ab33cb8f735d4c3a5f
蔡文莉 <a8568730@gmail.com>
2014-09-13 20:59:43 +0800

    $route+$routeParams+.config, 抓取網址上的參數給js

--- 海外頁面/static/js/first.js
+++ 海外頁面/static/js/first.js
@@ -1,6 +1,6 @@
 // Code goes here
 
-var app = angular.module("app1",['ui.bootstrap']);
+var app = angular.module("app1",['ui.bootstrap','ngRoute']);
 
 app.controller("LangController",["$scope", "$log","$http", function($scope, $log, $http){
 	$scope.langs = [];
@@ -18,19 +18,31 @@ app.controller("LangController",["$scope", "$log","$http", function($scope, $log
 	
 }]);
 
-app.controller("indexController",["$scope", "$log","$http", function($scope, $log, $http){
-	$scope.langs = ['lala'];
-	
-	$http({
-			method: 'GET',
-			url: '/語言表全部json',
-			data: {}
-	}).success(function(data, status) {
-			$scope.langs = data;
-			console.log(data);
-	});
-	
+app.controller("indexController",["$scope", "$log","$http","$routeParams", "$route", 
+	function($scope, $log, $http,$routeParams, $route){
+		$scope.langs = [];
+		
+		$http({
+				method: 'GET',
+				url: '/語言表全部json',
+				data: {}
+		}).success(function(data, status) {
+				$scope.langs = data;
+		});
+		//	inject $route and $routeParams to get url params 
+		$scope.param = $routeParams;
+		console.log($scope.param);
 }]);
+
+app.config(function($routeProvider, $locationProvider) {
+	  $routeProvider
+	   .when('/index/:language', {
+	    controller: 'indexController'
+	  });
+
+	  // configure html5 to get links working on jsfiddle
+	  $locationProvider.html5Mode(true);
+	});
 
 /* 解決 Django 和 AngularJS共用{{}}的混淆問題 */
 app.config(function($interpolateProvider) {
