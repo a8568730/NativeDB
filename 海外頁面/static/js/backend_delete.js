99e7bf28207835579f00958828eaa127bbf5624b
蔡文莉 <a8568730@gmail.com>
2014-10-26 22:07:57 +0800

    改用service匯入$http.get

--- 海外頁面/static/js/backend_delete.js
+++ 海外頁面/static/js/backend_delete.js
@@ -1,26 +1,38 @@
 var app1 = angular.module('app1',['ngRoute']);
 
-//app1.directive('deleteFile', function(){
-//	
-//});
-
-app1.controller('DeleteController', ['$scope', '$http', '$log', function($scope, $http, $log){
-	$scope.info = 2;
-	//得到檔案編號與檔名的list
-	$http({
-		method: 'GET',
-		url: '/語料的全部檔案json',
-		data: {},
-		params: {pk:$scope.info}
-	}).success(function(data, status) {
-		$scope.files = data;
-		console.log('files:'+$scope.files);
-	});
-	//
-}]);
-
-
 app1.config(function($interpolateProvider) {
 	$interpolateProvider.startSymbol('{$');
 	$interpolateProvider.endSymbol('$}');
+});
+
+app1.controller('DeleteController', ['$scope','FileService', function($scope, FileService){
+	var corpus = 2;
+	//得到檔案編號與檔名的list
+	$scope.files = FileService.GetFileList(corpus);
+}]);
+
+/*$scope不是service, 是Scope物件。不可放$scope在service*/
+app1.service('FileService', function($http, $log){
+	this.GetFileList = function(corpus){
+		var list=[];
+		console.log(corpus);
+		$http({
+			method: 'GET',
+			url: '/語料的全部檔案json',
+			data: {},
+			params: {pk: 2}
+		})
+		.success(function(data, status){
+			angular.forEach(data, function(line, key) {
+				  list.push(line);
+			});
+			console.log('files:'+ list);
+		});
+		return list;	
+	};
+	this.DeleteFile = function(){
+		//delete the file
+		
+		//get new list
+	};
 });
