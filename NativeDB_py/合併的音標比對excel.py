504efb3ea52059c0ff5798ff9b3b4de5054467b0
蔡文莉 <a8568730@gmail.com>
2014-12-06 15:33:06 +0800

    新增註解

--- NativeDB_py/合併的音標比對excel.py
+++ NativeDB_py/合併的音標比對excel.py
@@ -1,24 +1,25 @@
 from NativeDB_py.讀取EXCEL檔 import 把EXCEL讀進來
 # import xlrd
-
+# 先做完合併音節的位置.py
 def 音標比對excel(xlsx檔名, 合併音標):
 	excel音表 = 把EXCEL讀進來(xlsx檔名)
-	# 移掉表頭列 0th row = ['Num', 'Word', 'IPA', 'Note']
+	# 1. 移掉表頭列 0th row = ['Num', 'Word', 'IPA', 'Note']
 	excel音表.remove(excel音表[0])
 	
-	# 音標合併第二次，以便和xlsx陣列作比較
+	# 2. 音標合併第二次，以便和xlsx陣列作比較
 	對應表 = []
 	for 列 in 合併音標:
 		對應表.append(''.join(列[0]))
 		
-	# 紀錄沒有音檔或是可能標音錯誤的字
-# 	有存在音檔的字陣列 = []
+	# 3. 紀錄沒有音檔或是可能標音錯誤的字
+	# 合格的表：excel有對應到音檔，
+	# 合格的字格：標音正確，有找到對應的excel
 	合格的表 = []
 	不合格的表 = []
 	合格的字格 = []
 	不合格的字格 = []
 	
-	# 	1. 先找出excel的詞當中，有哪些的IPA不在textgrid裡
+	# 	4. 先找出excel的詞當中，有哪些的IPA不在textgrid裡
 	# 	順便紀錄合格的textgrid的索引
 	for 列 in excel音表:
 		if 列[2] in 對應表:
@@ -31,7 +32,7 @@ def 音標比對excel(xlsx檔名, 合併音標):
 			# 	不合格
 			不合格的表.append(列)
 		
-	# 	2. 檢查完excel後，也要挑出textgrid中沒有對應的IPA的，不合格的詞
+	# 	5. 檢查完excel後，也要挑出textgrid中沒有對應的IPA的，不合格的詞
 	for 索引 in range(len(對應表)):
 		if 索引 in 合格的字格:		
 				pass
@@ -39,7 +40,9 @@ def 音標比對excel(xlsx檔名, 合併音標):
 			不合格的字格.append(合併音標[索引])
 
 	if not len(不合格的表) == 0 or not len(不合格的字格) == 0:
+		print('找不到對應的textgrid的EXCEL字：有{0}個\n找不到對應的IPA的Textgrid：有{1}個\n'.format(len(不合格的表), len(不合格的字格)))
+		
 		raise RuntimeError(不合格的表, 不合格的字格)
 	
-	# 	3. 確認後，xlsx和textgrid的IPA應該一致
+	# 	6. 確認後，xlsx和textgrid的IPA應該一致
 	# 	return 'xlsx和textgrid的IPA目前檢查一致～～'
