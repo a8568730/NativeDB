8d11391e3bb95187665295bff6ecc894585d2605
蔡文莉 <a8568730@gmail.com>
2014-12-08 16:37:14 +0800

    加入檢查跳行而導致編號不連續的試驗 #17

--- 試驗/試驗_陣列轉json.py
+++ 試驗/試驗_陣列轉json.py
@@ -261,5 +261,24 @@ class 基本元素試驗(unittest.TestCase):
 		結果 = xlsx陣列轉json(問題, 詞數)
 		self.assertEqual(答案, 結果)
 		
+	def test_雙詞跳行(self):
+		問題 = [
+				['Num','Word','IPA','Note'],
+				['Zhang_VT_001','乖巧','kʰa3','kʰa3'],
+				['Zhang_VT_003','喔喔','kʰi3','kʰi3']
+			]
+		答案 =   [{"Num":"Zhang_VT_001",
+				"Word":"(喔)乖巧",	
+				"IPA":"kʰa3",
+				"Note":"kʰa3"},
+				{"Num":"Zhang_VT_003",
+				"Word":"喔喔()",	
+				"IPA":"kʰi1",
+				"Note":"kʰi1"},
+			]
+		詞數 = 2
+		結果 = xlsx陣列轉json(問題, 詞數)
+		self.assertEqual(答案, 結果)	
+		
 if __name__=='__main__':
 	unittest.main()
