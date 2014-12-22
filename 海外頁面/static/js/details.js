199e90111894c119215ded3a501dd5e8cd1b5517
蔡文莉 <a8568730@gmail.com>
2014-12-22 17:55:56 +0800

    把index.js從框一.html移到index.html

--- 海外頁面/static/js/details.js
+++ 海外頁面/static/js/details.js
@@ -7,12 +7,14 @@ app.directive('getUrlParams', function() {
 		//	取得 data-lang 的值為此網頁初次查看的語言
 	    attrs.$observe('lang', function(value) {
 	        $scope.lang = value;
-	    }).$observe('hangji', function(value) {
+	    });
+	    attrs.$observe('hangji', function(value) {
 	    	$scope.Hanji = value;
-	    }).$observe('ipa', function(value) {
+	    });
+	    attrs.$observe('ipa', function(value) {
 	    	$scope.IPA = value;
-	    }).;
-	}
+	    });
+	};
 });
 app.controller("detailController",["$scope", "$log", "$http", "$routeParams", "$route", "$location", "$window",
 	function($scope, $log, $http,$routeParams, $route, $location, $window){
@@ -44,15 +46,15 @@ app.controller("detailController",["$scope", "$log", "$http", "$routeParams", "$
 				method: 'GET',
 				url: $scope.lang + '/' + $scope.Hanji +  '/' + $scope.IPA + '/輸出同語言一漢字的所有音檔',
 				data: {}
-			}).success(function(data, status) {
+			})
+			.success(function(data, status) {
 				// 輸出格式 {	
 					//		word:字, 
 					//		IPA:音標, 
 					//		wavs: [{locate: 所在, age: 年歲, sex: 性別, wav: 音檔], {locate: 所在, age: 年歲, sex: 性別, wav: 音檔}], 
 					// 	}
 				$scope.contents=data;  
-				}
-			};
+			});
 		};
 		
 		//	切換其他分頁時，改網址，並讀資料
