e204100906ef25a6a65cbcba3d2f2f60cc0db60d
薛丞宏 <ihcaoe@gmail.com>
2015-05-17 16:39:11 +0800

    修正 合併重複的讀取EXCEL檔試驗

--- 試驗/試驗_讀取EXCEL檔.py
+++ 試驗/試驗_讀取EXCEL檔.py
@@ -17,7 +17,24 @@ class 基本元素試驗(unittest.TestCase):
 		結果 = 把EXCEL讀進來(試驗檔名)
 		self.assertEqual(結果, 答案)
 	
-	def test_textgrid(self):
+	def test_多個單音(self):
+		試驗檔名  = os.path.join(self.檔案所在 ,'numberbook.xlsx')
+		答案 = [		
+					['Num', 'Word', 'IPA', 'Note'],
+					['Zhang_VT_001', '(乖)巧', 'kʰa3', 'kʰa3'],
+					['Zhang_VT_002', '(肉)羹', 'kĩ1', 'kĩ1'],
+					['Zhang_VT_003', '富(人)', 'pu5', 'x'],
+					['Zhang_VT_004', '(醫)治', 'ti6', 'x'],
+					['Zhang_VT_005', '(乖)巧', 'kʰa3', 'kʰa3'],
+					['Zhang_VT_006', '(紅)茶', 'te2', 'te2'],
+					['Zhang_VT_007', '(土)地', 'te6', 'te6'],
+					['Zhang_VT_008', '(臭)殕(發霉)', 'pʰu3', 'pʰu3'],
+					['Zhang_VT_009', '(監)督', 'tok7', 'tok7']
+				]
+		結果 = 把EXCEL讀進來(試驗檔名)
+		self.assertEqual(答案, 結果)
+		
+	def test_工作表不可以超過一頁(self):
 		試驗檔名  = os.path.join(self.檔案所在 ,'Penang_M_M01_correct.xlsx')
 		答案 = '警告：excel檔的sheet超過一頁，請重新上傳'
 		結果 = 把EXCEL讀進來(試驗檔名)
@@ -32,6 +49,6 @@ class 基本元素試驗(unittest.TestCase):
 		]
 		結果 = 把EXCEL讀進來(試驗檔名)
 		self.assertEqual(結果, 答案)
-			
+				
 if __name__=='__main__':
 	unittest.main()
