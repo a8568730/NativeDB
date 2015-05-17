a5992e8a840c7fd98eef76d73bb263e329b8be16
薛丞宏 <ihcaoe@gmail.com>
2015-05-17 19:36:47 +0800

    刪除 寫出json檔.py（當初只是想看看Excel）

--- NativeDB_py/流程excel.py
+++ NativeDB_py/流程excel.py
@@ -1,16 +1,5 @@
 from NativeDB_py.讀取Excel檔 import 讀取Excel檔
 from NativeDB_py.檢查Excel檔格式與內容 import 檢查Excel檔格式與內容
-from NativeDB_py.寫出json檔 import 寫出json檔
-import json
-
-
-# def is_json(myjson):
-# 	try:
-# 		json_object = json.loads(myjson)
-# 	except:
-# 		return False
-# 	return True
-
 
 excel檔名 = 'numberbook2.xlsx'
 xlsx陣列 = 讀取Excel檔(excel檔名)
@@ -23,7 +12,6 @@ elif(len(xlsx陣列) != 0):
 	json陣列 = 檢查Excel檔格式與內容(xlsx陣列, 1)
 	if(type(json陣列[0]) is dict):
 		print('success', json陣列)
-		寫出json檔(json陣列, excel檔名)
 	else:	
 		# 輸出錯誤訊息
 		print('ohno', json陣列)
